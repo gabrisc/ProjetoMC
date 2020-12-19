4ab89299b6ad469f9f1ad24d13288611b90cbb3b
gabrisc <gc548354@gmail.com>
2020-12-19 16:46:43 -0300

    Primeira classe dominio

---
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.example.demo.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar(){
+		Categoria cat = new Categoria(1,"informatica");
+		Categoria cat2= new Categoria(2,"escritorio");
+		List<Categoria> listaCategorias= new ArrayList<Categoria>();
+		listaCategorias.add(cat);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
+ 		
+	}
+	
+	
+}
