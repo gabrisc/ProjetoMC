9d5e5d48c50e6c100db26b094a14107f751156ba
gabrisc <gc548354@gmail.com>
2020-12-20 01:52:19 -0300

    primeiro repository e service

--- src/main/java/com/example/demo/CursoMcApplication.java
+++ src/main/java/com/example/demo/CursoMcApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,5 @@ public class CursoMcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,27 +1,28 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar(){
-		Categoria cat = new Categoria(1,"informatica");
-		Categoria cat2= new Categoria(2,"escritorio");
-		List<Categoria> listaCategorias= new ArrayList<Categoria>();
-		listaCategorias.add(cat);
-		listaCategorias.add(cat2);
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}" , method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Categoria obj= service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 		
-		return listaCategorias;
  		
 	}
 	
-	
+	 
 }
